d29ac13dc4d3941cd52eab72caffb2cbd819abfd
yaxh.xv <yashnawgiri45@gmail.com>
2024-06-03 21:30:58 +0530

    added dropDown menu

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+ import CitySelector from './components/XStates'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ <CitySelector /> 
+
+     
+    </>
+  )
+}
+
+export default App
